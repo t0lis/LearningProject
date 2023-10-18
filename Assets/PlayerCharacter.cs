5504c28f0087b72b053189b0df4c15c6e51d84cb
t0lis <thebog96@gmail.com>
2023-10-18 20:51:36 +0700

    Shooting and hurt system implement
    Player now has HP and enemies can shoot projectiles.

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Health " + _health);
+    }
+}
--- Assets/WanderingAI.cs
+++ Assets/WanderingAI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WanderingAI : MonoBehaviour
 {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
 
@@ -27,7 +30,18 @@ public class WanderingAI : MonoBehaviour
 
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                if (hit.distance < obstacleRange)
+                GameObject hitObject = hit.transform.gameObject;
+
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange)
                 {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
